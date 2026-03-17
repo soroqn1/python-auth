ccbabb7fd09b7446a05505f3b9393e5fc9ff7516
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-03-17 10:09:54 +0200

    OAuth2 password bearer authentication and /users/me endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+
+from .security.auth import get_user
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/users/me")
+def read_user_me(user_token: str = Depends(get_user)):
+    return {"token_found": user_token, "message": "welcome"}
