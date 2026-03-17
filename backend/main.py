4a55af9029a783f202fbac6022a18e8a64856940
Yaroslav Sorochan <yaroslavsorocan89@gmail.com>
2026-03-17 10:25:28 +0200

    login endpoint add

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordRequestForm
 from security.auth import get_user
 
 app = FastAPI()
@@ -7,3 +8,9 @@ app = FastAPI()
 @app.get("/users/me")
 def read_user_me(user_token: str = Depends(get_user)):
     return {"token_found": user_token, "message": "welcome"}
+
+
+@app.post("/login")
+def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    # TODO: implement db check
+    return {"token": "test-token"}
